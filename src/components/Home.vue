8832dba2cbcc2e23aafa4ef563b19634ecc501d1
daiyifan <252127196@qq.com>
2021-11-15 13:06:02 +0800

    修改测试github

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,8 +16,8 @@
             <el-menu-item index="2-3">选项3</el-menu-item>
           </el-submenu>
           <el-submenu index="3">
-            <template slot="title"><i class="el-icon-setting"></i>导航三</template>
-            <el-menu-item index="3-1">选项1</el-menu-item>
+            <template slot="title"><i class="el-icon-setting"></i>管理配置中心</template>
+            <el-menu-item index="3-1">账号管理</el-menu-item>
             <el-menu-item index="3-2">选项2</el-menu-item>
           </el-submenu>
         </el-menu>
